408937bf4a6b2e25b761cca83626cfa31010a411
Shubham Raj <shubham14p3@gmail.com>
2020-08-21 00:49:07 +0530

    fixed linters

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Bookstore by Seth Zea and Shubham Raj</h1>
+    </div>
+  );
+}
+
+export default App;
