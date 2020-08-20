d56683f1b5915396bf98fc172969014442b78f67
sz16900 <sz16900@my.bristol.ac.uk>
2020-08-20 15:20:46 -0500

    implement App component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookList from '../containers/BookList';
+import BookList from '../containers/BookForm';
+
+const App = () => (
+  <div>
+    <BookList />
+    <BookForm />
+  </div>
+);
+
+export default App;
