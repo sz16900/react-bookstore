b2a2726393783751ecd04cf3b623eaffa9e74b95
Shubham Raj <shubham14p3@gmail.com>
2020-08-21 03:23:13 +0530

    added bookForm component

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,5 +1,19 @@
 import React from 'react';
+// import {connect} from 'react-redux';
 
-const BookForm = () => <div>hello</div>;
+const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+const BookForm = () =>
+// const [title,setTitle]=useState('');
 
+  (
+    <form>
+      <h2>Title:</h2>
+      <input name="title" type="text" />
+      <h2>Category:</h2>
+      <select name="category">
+        {categories.map(category => (<option key={category}>{category}</option>))}
+      </select>
+    </form>
+
+  );
 export default BookForm;
