10f57405222d02c9862a58f1ac04743c7e750278
Shubham Raj <shubham14p3@gmail.com>
2020-08-22 04:08:53 +0530

    remodified actions for forms

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,14 @@
-const createBook = (book) => ({
-  type: 'CREATE_BOOK',
+const CREATE_BOOK = 'CREATE_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
+
+const addBook = book => ({
+  type: CREATE_BOOK,
   book,
 });
 
-const removeBook = (book) => ({
-  type: 'REMOVE_BOOK',
-  book,
+const removeBook = id => ({
+  type: REMOVE_BOOK,
+  id,
 });
 
-export default { createBook, removeBook };
+export { addBook, removeBook };
