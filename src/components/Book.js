1729c5b1df8444597e11f88322a70f31ca0729cf
sz16900 <sz16900@my.bristol.ac.uk>
2020-08-21 12:23:37 -0500

    modified files in order to make components work

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,22 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Book = ({ book }) => {
-  const {
-    id,
-    title,
-    category,
-  } = book;
+  const { id, title, category } = book;
 
   return (
-    <>
+    <tr>
       <td>{id}</td>
       <td>{title}</td>
       <td>{category}</td>
-    </>
+    </tr>
   );
 };
 
 Book.propTypes = {
-  book: PropTypes.objectOf(PropTypes.object).isRequired,
+  book: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    category: PropTypes.string,
+  }).isRequired,
 };
 export default Book;
