2c0ff22d77187426b2a466b1b4111f44e4221415
sz16900 <sz16900@my.bristol.ac.uk>
2020-08-21 11:39:38 -0500

    add books reducer

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,1 +1,10 @@
-//
+const booksReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'CREATE_BOOK':
+      return [...state, action.payload];
+    case 'REMOVE_BOOK':
+      return state.filter((book) => book.id !== action.payload.id);
+    default:
+      return state;
+  }
+};
