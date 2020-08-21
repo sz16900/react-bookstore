c2dff69b4ad98100b3f830bc034a1234572801de
sz16900 <sz16900@my.bristol.ac.uk>
2020-08-21 11:27:36 -0500

    create actions/index.js

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,19 +1,24 @@
 import React from 'react';
-// import {connect} from 'react-redux';
 
-const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-const BookForm = () =>
-// const [title,setTitle]=useState('');
-
-  (
-    <form>
-      <h2>Title:</h2>
-      <input name="title" type="text" />
-      <h2>Category:</h2>
-      <select name="category">
-        {categories.map(category => (<option key={category}>{category}</option>))}
-      </select>
-    </form>
-
-  );
+const categories = [
+  'Action',
+  'Biography',
+  'History',
+  'Horror',
+  'Kids',
+  'Learning',
+  'Sci-Fi',
+];
+const BookForm = () => (
+  <form>
+    <h2>Title:</h2>
+    <input name="title" type="text" />
+    <h2>Category:</h2>
+    <select name="category">
+      {categories.map((category) => (
+        <option key={category}>{category}</option>
+      ))}
+    </select>
+  </form>
+);
 export default BookForm;
