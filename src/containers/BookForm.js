c1120703eb6318db31195fbeaef6f5e90efe0b07
Shubham Raj <shubham14p3@gmail.com>
2020-08-22 01:22:08 +0530

    added implemented the bookForm Logic

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { createBook } from '../actions/index';
+import createBook from '../actions/index';
 
 const categories = [
   'Action',
@@ -12,6 +12,8 @@ const categories = [
   'Learning',
   'Sci-Fi',
 ];
+let idGen=3;
+
 const BookForm = (props) => {
   const [title, setTitle] = useState('');
   const [category, setCategory] = useState('Action');
@@ -24,7 +26,14 @@ const BookForm = (props) => {
     }
   };
 
-  <form>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const book={id: idGen += 1, title, category};
+    props.submitNewBook(book);
+    event.target.reset();
+  };
+return(
+  <form onSubmit={handleSubmit}>
     <h2>Title:</h2>
     <input name="title" type="text" value={title} onChange={handleChange} />
     <h2>Category:</h2>
@@ -33,17 +42,19 @@ const BookForm = (props) => {
         <option key={category}>{category}</option>
       ))}
     </select>
-  </form>;
+    <button type="submit">Submit</button>
+  </form>
+);
 };
 
-const mapDispatchToProps = (state) => ({
-  book: () => {
+const mapDispatchToProps = (dispatch) => ({
+  submitNewBook: book => {
     dispatch(createBook(book));
   },
 });
 
 BookForm.propTypes = {
-  book: PropTypes.func.isRequired,
+  submitNewBook: PropTypes.func.isRequired,
 };
 
 export default connect(null, mapDispatchToProps)(BookForm);
