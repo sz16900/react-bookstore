b5309e6119bab5fedd35a0c00e9f5d594c55c439
sz16900 <sz16900@my.bristol.ac.uk>
2020-08-21 14:34:52 -0500

    bookform handle changes

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createBook } from '../actions/index';
 
 const categories = [
   'Action',
@@ -9,16 +12,40 @@ const categories = [
   'Learning',
   'Sci-Fi',
 ];
-const BookForm = () => (
+const BookForm = (props) => {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('Action');
+
+  const handleChange = (event) => {
+    if (event.target.name === 'title') {
+      setTitle(event.target.value);
+    } else {
+      setCategory(event.target.value);
+    }
+  };
+
   <form>
     <h2>Title:</h2>
-    <input name="title" type="text" />
+    <input name="title" type="text" value={title} onChange={handleChange} />
     <h2>Category:</h2>
-    <select name="category">
-      {categories.map(category => (
+    <select name="category" value={category} onChange={handleChange}>
+      {categories.map((category) => (
         <option key={category}>{category}</option>
       ))}
     </select>
-  </form>
-);
-export default BookForm;
+  </form>;
+};
+
+const mapDispatchToProps = (state) => ({
+  book: () => {
+    dispatch(createBook(book));
+  },
+});
+
+BookForm.propTypes = {
+  book: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(BookForm);
+
+// export default BookForm;
