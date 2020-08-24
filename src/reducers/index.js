c7486455329b659c7e5862715ebf6ab7624e1437
sz16900 <sz16900@my.bristol.ac.uk>
2020-08-24 11:01:50 -0500

    combine filter to reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import books from './books';
-
-const rootReducer = combineReducers({ books });
+import filter from './filter';
+const rootReducer = combineReducers({ books, filter });
 
 export default rootReducer;
