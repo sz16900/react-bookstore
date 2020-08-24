427027bddea000248192340c6e993b811a27cd78
sz16900 <sz16900@my.bristol.ac.uk>
2020-08-24 10:38:30 -0500

    add actions filter

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,20 @@
 const CREATE_BOOK = 'CREATE_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
+const CHANGE_FILTER = 'CHANGE_FILTER';
 
-const addBook = book => ({
+const addBook = (book) => ({
   type: CREATE_BOOK,
   book,
 });
 
-const removeBook = id => ({
+const removeBook = (id) => ({
   type: REMOVE_BOOK,
   id,
 });
 
-export { addBook, removeBook };
+const changeFilter = (option = 'All') => ({
+  type: CHANGE_FILTER,
+  option,
+});
+
+export { addBook, removeBook, changeFilter };
